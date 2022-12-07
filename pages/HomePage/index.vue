44852ad55e9ad050fd5a42a04496dd636de9315a
mirzahanmirzahanov <www.mirzahanmirzahanov@gmail.com>
2022-12-07 23:33:19 +0300

    init commit

---
+++ pages/HomePage/index.vue
@@ -0,0 +1,22 @@
+<template>
+<div class='home-page'>
+</div>
+</template>
+
+
+<script>
+
+export default {
+components: {},
+name: 'HomePage', 
+computed: {},
+props: {},
+data: () => ({}),
+methods: {}
+}
+</script>
+
+
+<style lang='scss' scoped>
+
+</style>
